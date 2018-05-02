4668f09cb498ae14ecb666b01be5d18a237b71fe
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-02 01:27:09 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,10 +22,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.each do |elements|
-    swap = string[2]
-    string[2] = string[$]
-    string[2] = swap
-    array
+  array.each do |element|
+    element[2] = "$"
   end
 end
