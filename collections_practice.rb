a293a1fc1d5639b6cd80cab43089eefcc99fdb42
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-02 02:07:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { |element, index| element = element + "s" if index != 1 }
+  array.each_with_index.collect { |element, index|
+    if index != 1
+      element + "s"
+    else
+      element
+    end
+  }
 end
